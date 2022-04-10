ffd35a196b5a9065293d43f572e4301d2fbb3166
Diarmuid McGonagle <diarmuidmcgonagle@gmail.com>
2022-04-10 15:54:41 +0100

    created TST object w boilerplate

---
+++ src/com/company/HelperClasses/TST.java
@@ -0,0 +1,15 @@
+package com.company.HelperClasses;
+
+public class TST {
+    private int size;
+    private Node rootNode;
+
+    private static class Node {
+        private char currentChar;
+        private Node leftTree, midTree, rightTree;
+        private Stop stopValue;
+    }
+
+    public TST() {
+    }
+}
